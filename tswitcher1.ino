aceb2d9a911f0ea138c48422582172a99ee7044c
Tony Muka <tonymuka@gmail.com>
2012-10-06 01:32:07 -0400

    setting up input pins in arrays instead on individually, stubbing out some functions

--- tswitcher1.ino
+++ tswitcher1.ino
@@ -12,21 +12,25 @@
 elapsedMillis sinceInit;
 
 #define NUM_BUTTONS 2
-#define NUM_RELAYS 1
+#define NUM_RELAYS 2
 #define NUM_RGB_LEDS 1
 
 #define DEBOUNCE_MS 5
 
 //define the single coil latching relay pins in a multi-dimensional array. remember this is zero indexed.
 int relayPins[NUM_RELAYS][2] = {
-  { 
-    9,10     }
+  {  
+    9,10       }
+  ,
+  {  
+    9,10       }
 };
 int buttonPins[NUM_BUTTONS] = { 
   7,8 };
 
 int rgbLEDpins[NUM_RGB_LEDS][3] = {
-  { 26,25,24 }
+  { 
+    26,25,24   }
 };
 
 #define DURATION_BLINK 2000
@@ -36,9 +40,10 @@ int rgbLEDpins[NUM_RGB_LEDS][3] = {
 // Last arg=true indicates that all of these pins are PWM enabled, omit for no PWM
 // If you don't specify this and then setPwm, the library sets the LED to WHITE
 
-int relayStates[NUM_RELAYS] = {0}; //inits the state array to all zeros
+int relayStates[NUM_RELAYS] = {
+  0}; //inits the state array to all zeros
 
-int thisPin = 0; //init this we'll use in loops below
+//int thisPin = 0; //init this we'll use in loops below
 
 // Instantiate a Bounce object with a 5 millisecond debounce time
 // couldn't figure out how to make this code dynamic for NUM_BUTTONS
@@ -49,7 +54,7 @@ Bounce bouncer[NUM_BUTTONS] = {
 
 RgbLedCommonAnode rgb_led[NUM_RGB_LEDS] = {
   RgbLedCommonAnode( rgbLEDpins[0][0], rgbLEDpins[0][1], rgbLEDpins[0][2], true )
-};
+  };
 
   void setup()   {
     Serial.begin(38400);
@@ -57,12 +62,12 @@ RgbLedCommonAnode rgb_led[NUM_RGB_LEDS] = {
     rgb_led[0].setColor(0, 15, 5); //so we can tell init is happening
 
     //init button input pins with pullup
-    for (thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
+    for (int thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
       pinMode(buttonPins[thisPin], INPUT_PULLUP);
     }
 
     //init relay pins as outputs
-    for (thisPin = 0; thisPin < NUM_RELAYS; thisPin++)  {
+    for (int thisPin = 0; thisPin < NUM_RELAYS; thisPin++)  {
       pinMode(relayPins[thisPin][0], OUTPUT);      
       pinMode(relayPins[thisPin][1], OUTPUT);      
     }
@@ -75,27 +80,40 @@ RgbLedCommonAnode rgb_led[NUM_RGB_LEDS] = {
 void loop()                     
 {
 
-  for (thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
+  for (int thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
     //Serial.print("checking pin #");
     //Serial.println(thisPin,DEC);
     // Update the debouncer
     if(bouncer[thisPin].update()){
-      // Get the update value
-      if(bouncer[thisPin].read() == LOW){
-        relayStates[thisPin] = flipBit(relayStates[thisPin]); //change relay state for active pin
+      if(bouncer[thisPin].read() == LOW){        // Get the update value
         Serial.print("Button #");
         Serial.print(thisPin,DEC);
         Serial.println(" pressed");
-        setRelay(thisPin,relayStates[thisPin]);
+        setRelay(thisPin,flipBit(relayStates[thisPin]));
+        debugStatus();
       }
+      if(bouncer[thisPin].fallingEdge()){ 
+        Serial.println("falling edge");
+      }
+      if(bouncer[thisPin].risingEdge()){ 
+        Serial.println("rising edge"); 
+        //Serial.println(bouncer[thisPin].duration());
+      }
+
+      // check for button chording (two buttons at the same time)
+      // we could use that to change modes, etc.
+
+      // check for button long press (>3s?) to save preset in preset mode
+      // maybe in stomp mode a long press excludes a button from preset mode presets
+
+      // perhaps make subsequent presses of a button in preset mode set tap tempo
+      // verify that tempo is within a valid useful range, say 40-200bpm (presto!).
+
     }
-
-    //if(bouncer2.risingEdge())
-
   }
 
   // change this to slow down the looping.
-  //delay(50);
+  delay(100);
 
 }
 
@@ -110,6 +128,12 @@ int setRelay(int relayNum, int state){
   Serial.print(relayNum,DEC);
   Serial.print(" to ");
   Serial.println(state);
+
+  if(relayNum > NUM_RELAYS){
+    Serial.println("ERROR: requested relayNum does not exist!!!");
+    return 0; 
+  }
+
   if(state){
     digitalWrite(relayPins[relayNum][0],HIGH);  
     digitalWrite(relayPins[relayNum][1],LOW);
@@ -121,10 +145,12 @@ int setRelay(int relayNum, int state){
     rgb_led[relayNum].setColor(Color::YELLOW);
   }
   delay(1);
+  relayStates[relayNum] = state; //update the state array
 
   //done toggling, set both pins idle
   digitalWrite(relayPins[relayNum][0],LOW);  
   digitalWrite(relayPins[relayNum][1],LOW);
+  return 1;
 }
 
 
@@ -138,28 +164,28 @@ void storePreset(int preset_number, int relayStates){
 
   EEPROM.write(preset_number, relayStates);
 }
-  /*
+/*
 //recall a preset number containing a number 0-255 representing the 8bit byte of relay settings to store.
-void recallPreset(int preset_number){
-
-  // read a byte from the current address of the EEPROM
-  int relayStatesByte = EEPROM.read(preset_number);
-  if(!relayStatesByte){ 
-    relayStatesByte = 255;  //default to all on with 255, or all off with 0
-  }
-  Serial.print("loadPreset #");   
-  Serial.print(preset_number);
-  Serial.print("\t");
-  Serial.print(relayStatesByte, BIN);
-  Serial.println();
-
-  //convert relayStates from byte to array
-  for (int thisPin = 0; thisPin < NUM_RELAYS; thisPin++)  {
-    relayStates[thisPin] = DEC relayStatesByte[thisPin];
-  }
-  return relayStates;
-}
-  */
+ void recallPreset(int preset_number){
+ 
+ // read a byte from the current address of the EEPROM
+ int relayStatesByte = EEPROM.read(preset_number);
+ if(!relayStatesByte){ 
+ relayStatesByte = 255;  //default to all on with 255, or all off with 0
+ }
+ Serial.print("loadPreset #");   
+ Serial.print(preset_number);
+ Serial.print("\t");
+ Serial.print(relayStatesByte, BIN);
+ Serial.println();
+ 
+ //convert relayStates from byte to array
+ for (int thisPin = 0; thisPin < NUM_RELAYS; thisPin++)  {
+ relayStates[thisPin] = DEC relayStatesByte[thisPin];
+ }
+ return relayStates;
+ }
+ */
 void activatePreset(int preset_number){
   //setAllRelays(recallPreset(preset_number));
   Serial.print("activating Preset #");
@@ -168,7 +194,7 @@ void activatePreset(int preset_number){
 }
 
 void printAllPresets(){
-  for (thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
+  for (int thisPin = 0; thisPin < NUM_BUTTONS; thisPin++)  {
     Serial.print("Preset #"); 
     Serial.print(thisPin);
     Serial.print(" - ");
@@ -190,4 +216,15 @@ void setAllLEDs(int relayStates){
 
 }
 
+void debugStatus(){
+  for(int thisPin = NUM_RELAYS-1; thisPin >= 0; thisPin--) {
+    Serial.print(relayStates[thisPin],DEC); 
+  }
+  Serial.println();
+}
+
+//send a few pulses to the tap tempo jack at the specified bpm
+void setTapTempo(int bpm){
+  //do we need a relay for this, or just a digital pin with the pulldown resistor?
+}
 
